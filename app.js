9914e0db88dd866b4d4e474d2d267832a59d1f80
shajidulashfak <shajidulashfak@gmail.com>
2021-04-29 05:24:02 +0600

    Bank Transaction

---
+++ app.js
@@ -0,0 +1,63 @@
+      //submit button Handler ;
+
+      var loginBtn = document.getElementById("login");
+      loginBtn.addEventListener("click", function(){
+          var loginArea = document.getElementById("login-area");
+          loginArea.style.display = "none";
+          var transactionArea = document.getElementById("transaction-area");
+          transactionArea.style.display = "block";
+      });
+      
+      // Deposit Button Handler ;
+      var depositBtn = document.getElementById("deposit");
+      depositBtn.addEventListener("click", function(){
+          var depositAmount = document.getElementById("depositAmount").value;
+          var depositNumber = parseFloat(depositAmount);
+        if (depositNumber < 0 ){
+              alert("Deposit Number Can't be Negative.");
+        }
+        else{
+          updateSpan("currentDeposit", depositNumber);
+          updateSpan("currentBalance", depositNumber);
+
+          document.getElementById("depositAmount").value = "";
+
+        }
+          // var currentDeposit = document.getElementById("currentDeposit").innerText;
+          // var currentDepositNumber = parseFloat(currentDeposit);
+          // var totalDeposit = depositNumber + currentDepositNumber;
+          
+          // document.getElementById("currentDeposit").innerText = totalDeposit;
+
+          // var currentBalance = document.getElementById("currentBalance").innerText;
+          // var currentBalanceNumber = parseFloat(currentBalance);
+          // var totalBalance = depositNumber + currentBalanceNumber;
+          // document.getElementById("currentBalance").innerText = totalBalance;
+      })
+
+      function updateSpan(id, depositNumber){
+          var current = document.getElementById(id).innerText;
+          var currentNumber = parseFloat(current);
+          var totalAmount = depositNumber + currentNumber;
+          document.getElementById(id).innerText = totalAmount;
+      }
+
+      // Withdraw Button Handler ;
+      var withdrawBtn = document.getElementById("withdraw");
+      withdrawBtn.addEventListener("click", function(){
+          var withdrawAmount = document.getElementById("withdrawAmount").value;
+          var withdrawNumber = parseFloat(withdrawAmount);
+      })
+
+      // var withdrawBtn = document.getElementById("withdraw");
+      // withdrawBtn.addEventListener("click", function(){
+      //     var withdrawAmount = document.getElementById("withdrawAmount").value;
+      //     var withdrawNumber = parseFloat(withdrawAmount);
+          
+      //     var currentWithdraw = document.getElementById("currentWithdraw").innerText;
+      //     var currentWithdrawNumber = parseFloat(currentWithdraw);
+      //     var totalWithdraw = currentNumber + currentWithdrawNumber;
+
+          document.getElementById("withdrawAmount").value = "";
+      // })
+   
